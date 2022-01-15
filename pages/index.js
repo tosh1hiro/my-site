f4ce423f0567b63d1a8608c136a67fff0c75f49d
user20201220 <t-sugahara@3sss.co.jp>
2022-01-16 03:35:38 +0900

    ヘッダーの画像、メニューを追加

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-      <h1 className='text-blue-700'>テスト</h1>
-    </>  
+      <header>
+        <div className='max-w-screen-lg mx-auto h-12 clear-both' style={{ position: 'relative', height: 100, width: '100%' }}>
+          <div className='float-left' style={{ position: 'relative', height: '100%', width: 200 }}>
+            <Image src='/../public/images/logo.png' layout='fill' alt='ロゴ' />
+          </div>
+          <div className='float-right top-1/2 bottom-1/2' style={{ position: 'relative', width: 'auto' }}>
+            <ul className=' clear-both'>
+              <li className='float-left ml-6'><a href="#" className="text-regal-blue">ABOUT</a></li>
+              <li className='float-left ml-6'><a href="#">SKILLS</a></li>
+              <li className='float-left ml-6'><a href="#">VALUES</a></li>
+              <li className='float-left ml-6'><a href="#">FUTURE</a></li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </>
   )
 }
